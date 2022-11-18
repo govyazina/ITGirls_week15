68fbac840dd7d312d880d0f907e14543c47b3c3f
Svetlana Govyazina <govyazina@gmail.com>
2022-11-18 21:33:32 +0200

    functions for inputs added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,3 +13,36 @@ function showOtherInput (e) {
        document.querySelector(".other-input-find").setAttribute("class", "other-input-find other-input")
    }
 }
+
+function checkLisense () {
+    return document.querySelector("#lisense").checked;
+
+}
+
+(function () {
+    document.querySelector("form").addEventListener('submit', submit);
+    let a = document.querySelectorAll(".required");
+    a.forEach(function (elem) {
+        elem.addEventListener('blur', input)
+    })
+})();
+
+function getInputValue(elem) {
+    return elem.value;
+}
+
+function input(event) {
+    let value = getInputValue(event.target);
+    let valid = validateRequired(value);
+    if (!valid) {
+        alert(`заполни поле ${event.target.name}`)
+    }
+}
+
+function submit(event) {
+    if (!checkLisense()) {
+        alert('прими лицензионное соглашение')
+        return false;
+    }
+
+}
