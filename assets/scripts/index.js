98f389b5474f20b25d3c87b7350b7e5b0ee7eb9e
Svetlana Govyazina <govyazina@gmail.com>
2022-11-19 15:14:38 +0200

    functions for form submit add

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,21 +1,26 @@
-function validateRequired (value) {
+function validateRequired(value) {
     if (value === "") {
         return false
     }
     return true;
 }
 
-function showOtherInput (e) {
-   if (e.target.id === 'from-other') {
-       document.querySelector(".other-input-find").setAttribute("class", "other-input-find")
-   }
-    else {
-       document.querySelector(".other-input-find").setAttribute("class", "other-input-find other-input")
-   }
+function showOtherInput(e) {
+    if (e.target.id === 'from-other') {
+        document.querySelector(".other-input-find").setAttribute("class", "other-input-find")
+    } else {
+        document.querySelector(".other-input-find").setAttribute("class", "other-input-find other-input")
+    }
 }
 
-function checkLisense () {
-    return document.querySelector("#lisense").checked;
+function checkLisense() {
+    let check = document.querySelector("#lisense").checked;
+    if (check) {
+        return check;
+    } else {
+        alert('прими лицензионное соглашение');
+        return check;
+    }
 
 }
 
@@ -23,7 +28,9 @@ function checkLisense () {
     document.querySelector("form").addEventListener('submit', submit);
     let a = document.querySelectorAll(".required");
     a.forEach(function (elem) {
-        elem.addEventListener('blur', input)
+        elem.addEventListener('blur', function (e) {
+            input(e.target);
+        })
     })
 })();
 
@@ -31,18 +38,34 @@ function getInputValue(elem) {
     return elem.value;
 }
 
-function input(event) {
-    let value = getInputValue(event.target);
+function input(elem) {
+    let value = getInputValue(elem);
     let valid = validateRequired(value);
     if (!valid) {
-        alert(`заполни поле ${event.target.name}`)
+        alert(`заполни поле ${elem.name}`)
     }
+    return valid;
 }
 
 function submit(event) {
-    if (!checkLisense()) {
-        alert('прими лицензионное соглашение')
-        return false;
+    if (!(checkLisense() && validateRequiredFields())) {
+        event.preventDefault();
     }
+    else {
+        alert (`Привет, ${getUserName()}!`)
+    }
+}
 
+function getUserName() {
+    return document.querySelector('#first-name').value;
+
+}
+
+function validateRequiredFields() {
+    let check = true;
+    let a = document.querySelectorAll(".required");
+    a.forEach(function (elem) {
+        check = check && input(elem);
+    })
+    return check;
 }
